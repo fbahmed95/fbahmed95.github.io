ab1d0285814205be25b724f41e0c43aa51dd9486
Fareeha Ahmed <fareeha@Fareehas-MacBook-Pro.local>
2020-08-09 13:35:52 -0700

    Switched to portfolio

--- main.js
+++ main.js
@@ -1,91 +1,84 @@
-// load cards
-window.addEventListener('load', (event) => {
-    getCards(16)
-    var clicked = {};
-    var data1 = -1; 
-    var data2 = -1;
-    $(".flip-card").on("click", function(){
-        // if($(".flip-animation").length < 2 ){
-        //     $(this).find(".flip-card-inner").addClass("flip-animation");
-        // } else {
-        //     $(".flip-card-inner").removeClass("flip-animation");
-        // }
-        if(data1 === -1 || data2 === -1){
-            $(this).find(".flip-card-inner").addClass("flip-animation");
-            if(data1 !== -1){
-                data2 = $(this).find(".flip-card-inner").data("type");
-            } else {
-                data1 = $(this).find(".flip-card-inner").data("type");
-                console.log("type", $(this).find(".flip-card-inner").data("type"))
-            }
-            console.log(data1, data2);
-                if(data2 > -1 && data1 > -1){
-                    if(data2 !== data1){
-                        setTimeout(function(){ 
-                            console.log("removing")
-                            $(".flip-card-inner[data-type='"+data1+"'").removeClass("flip-animation");
-                            $(".flip-card-inner[data-type='"+data2+"'").removeClass("flip-animation");
-                            data1 = -1; 
-                            data2 = -1;        
-                        }, 1000); 
-                    } else {
-                        data1 = -1; 
-                        data2 = -1;
-                    }
-                }
-        }
+var i = 0;
+var txt = "Type 'command' to get a list of commands"; /* The text */
+var speed = 80; /* The speed/duration of the effect in milliseconds */
 
-        // var data = $(this).find("flip-card-back").data("type");
-        // if($(".flip-animation").length == 2 && $().length !=2){
+function typeWriter() {
+  if (i < txt.length) {
+    //$("#terminal-input").val($("#terminal-input").val() + txt.charAt(i));
+    $("#terminal-input").attr("placeholder", $("#terminal-input").attr("placeholder")+ txt.charAt(i))
+    i++;
+    $("#terminal-input").attr('size', $("#terminal-input").attr("placeholder").length);
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter();
 
-        // }
-        
-    })
+function resizeInput() {
+    if($(this).val()){
+        $(this).attr('size', $(this).val().length);
+    } else {
+        $(this).attr('size', $(this).attr("placeholder").length);
+    }
+}
+
+$('#terminal-input')
+    // event handler
+    .keyup(resizeInput)
+    // resize on page load
+    .each(resizeInput);
+
+var commands_text = {
+    "command" : `--about --github --experience --skills --resume`,
+    "--about" : `Hey! My name is Fareeha Ahmed. I am an full-stack engineer
+                based in Bay Area, CA. When I'm not writing code, I'm probably 
+                painting, watching crime shows, or munching on hot cheetos. Check
+                my projects for more!`,
+
+    "--github" : `<a href="https://github.com/fbahmed96" target="_blank">fbahmed95</a>`,
+    "--experience" : `Three months as an intern at GSVlabs. 1.5 years as a software engineer
+                     at GSVlabs. One month as team lead.`,
+    "--skills" : `Leadership qualities, Javascript, Node.js`,
+    "--resume" : `Go to my linkedin`,
+
+}
+
+const generateTerminalText = (text) => {
+    return `<div class="terminal" id="terminal-outer-container">
+            <div class="terminal terminal-content-container">
+                <div class="terminal terminal-content">${text}</div>
+            </div>
+        </div>`
+}
+const generateCommandText = (text) => {
+    return `<div class="terminal" id="terminal-outer-container">
+            <div class="terminal terminal-content-container">
+                <span class="terminal terminal-prepend"></span>
+                <div class="terminal terminal-content">${text}</div>
+            </div>
+        </div>`
+}
+
+$("#terminal-input-form").submit(function( event ) {
+    event.preventDefault();
+    let command = $("#terminal-input").val();
+    let terminal_command_text = generateCommandText(command);
+    $("#terminal-input").val("");
+    $("#terminal-input").attr("placeholder", "");
+    command = commands_text[command];
+    let terminal_text = command ? generateTerminalText(command) : generateTerminalText("this command does not exist");
+    $("#terminal-outer-container").html($("#terminal-outer-container").html() + terminal_command_text + terminal_text);
   });
 
-function getRandomInt(max) {
-return Math.floor(Math.random() * Math.floor(max));
-}
-
-function getCards(numCards){
-    var cardContainer = $("#card-container");
-    var html = "";
-    // get random number using array length
-    // remove index from array 
-    // get new random number using array length 
-    var base_arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
-    var rand_arr_index = getRandomInt(base_arr.length); 
-    var rand_arr; 
-    if(rand_arr_index + (numCards/2) > base_arr.length){
-        var arr1 = base_arr.slice(rand_arr_index, base_arr.length); 
-        var arr2 = base_arr.slice(0, (numCards/2) - arr1.length);
-        rand_arr = arr1.concat(arr2);
+$("input[name=toggle]").on("click", function(){
+    let checked = $(this).is(':checked');
+    if(checked){
+        $("#light").show();
+        $("#dark").hide();
+        $(".terminal").removeClass("dark")
     } else {
-        rand_arr = base_arr.slice(rand_arr_index, rand_arr_index + (numCards/2))
+        $("#light").hide();
+        $("#dark").show();
+        $(".terminal").addClass("dark")
     }
-    var used = {};
-    for(var i = 0; i < numCards; i++){
-        var random_index = getRandomInt(rand_arr.length);
-        var data = rand_arr[random_index];
-         if (used[data] === 1){
-            used[data] += 1;
-            rand_arr.splice(random_index, 1);
-        } else {
-            used[data] = 1;
-        }
-        html += `<div class="flip-card">
-        <div class="flip-card-inner" data-type="`+data+`">
-          <div class="flip-card-front">
-            <!-- image goes here -->
-          </div>
-          <div class="flip-card-back">
-          <!-- image goes here -->
-          `+data+`
-          </div>
-        </div>
-      </div>
-      `
-    }
-    cardContainer.html(html);
-}
-
+    console.log("switch", $(this).is(':checked'))
+})
